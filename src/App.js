e5b6484f7b1303e90c7fbd57f4ef37a8fcc1260f
Abdrlrazzaq Asfopur <130183951+Abdelrazzaq-Asfour@users.noreply.github.com>
2023-10-01 22:31:30 +0300

    add search and router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './header';
 import Main from './Main';
+import { Card } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import Products from './products';
 
 function App() {
   return (
     <>
       <Header  />
-      <Main />
 
+      <Router>
+      <Routes>
+        <Route  path="/" Component={Main}></Route>
+          <Route   path="/products" Component={Products}></Route>
+      </Routes>
+      </Router>
+     
+      
     </>
   );
 }
