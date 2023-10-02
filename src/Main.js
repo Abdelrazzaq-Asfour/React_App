7447723b1883b7426fc3c1b5ac3488ec385f220c
Abdrlrazzaq Asfopur <130183951+Abdelrazzaq-Asfour@users.noreply.github.com>
2023-10-02 05:45:02 +0300

    add data

--- src/Main.js
+++ src/Main.js
@@ -1,55 +1,66 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import  data from './data.json';
-import cardComp from './card';
-import Modal from 'react-bootstrap/Modal';
-import { useState } from 'react';
-import CardCamp from './card';
+import CardComp from './card';
+import './main.css';
+import { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
-
-
-
+import Button from 'react-bootstrap/Button';
 
 function Main (){
-  let [items, setItems]= useState([data]);
-   function handleSubmit(event){
-    event.preventDefault()
-      let searchedValue = event.target.search.value
-    let filteredItems = data.filter(function(item){return  item.title.toLowerCase().includes(searchedValue.toLowerCase())})
-   setItems(filteredItems);
+    let [items, setItems] = useState([]);
+
+   async function getData(){
+
+    const url = 'https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=us&lang=en&currentpage=0&pagesize=30&categories=men_all&concepts=H%26M%20MAN';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '0f2abd4395msh6ce1c28ccac1aacp178ddbjsn9cd63c10ba80',
+        'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setItems(result.results)
+    } catch (error) {
+      console.error(error);
     }
+}
+
+useEffect(function (){getData()}, [])
+
+    // function handleSubmit (event){
+    //     event.preventDefault()
+    //     let searchedValue = event.target.search.value;
+        
+    //     let filteredItems = data.filter(function(item){return item.title.toLowerCase().includes(searchedValue.toLowerCase() )})
+    //     setItems(filteredItems);
+    // }
     return(
         <>
-        <Form className="d-flex" onSubmit={handleSubmit}>
+          <Form className="d-flex"  id="myform">
             <Form.Control
               type="search"
               placeholder="Search"
               className="me-2"
               aria-label="Search"
-              name ="Search"
-            />
-            <Button variant="outline-success" type='submit' >Search</Button>
+              name="search"
+              />
+            <Button variant="outline-success" type='submit'>Search</Button>
           </Form>
-          <div id="container">
-
+        <div id="container">
         {items.map(function(item){
-         
-    return(
-
-        <CardCamp image={item.image_url} title={item.title} description= {item.description} price={item.price} />
-         
-
-           )
-}
+            return(
+              <>
+                <CardComp image={item.images[0].baseUrl} title={item.name} price={item.price.value}/>
+                </>
+            )
+        }
     )
-    }
-    </div>
+        }
+        </div>
         </>
     )
 }
+
 export default Main;
-
-
-
-
-// <div style={{display:"flex",flexWrap:"wrap",justifyContent:"space-between",gap:'2px',marginTop:"3%"}}>
